301e372335718586a3fb3886fb305eb1550520bc
unknown <fhalasahhh@gmail.com>
2020-06-29 20:07:08 +0300

    Updates

---
+++ precache-manifest.33512f40558748571522d7fa09a78bc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116eeaa362137b0ad235b33ea7f1ddb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "551360d26db8e74f7d81",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bf11d1e54aada0f07157",
+    "url": "/static/css/main.163ec550.chunk.css"
+  },
+  {
+    "revision": "551360d26db8e74f7d81",
+    "url": "/static/js/2.9c85c9a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c85c9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf11d1e54aada0f07157",
+    "url": "/static/js/main.a51da8ec.chunk.js"
+  },
+  {
+    "revision": "aedd34daf8a357f4b19d",
+    "url": "/static/js/runtime-main.e56ae63b.js"
+  },
+  {
+    "revision": "e2d4fc3526ad3a35bc178b326f3cdbf6",
+    "url": "/static/media/ISS.e2d4fc35.jpg"
+  }
+]);
